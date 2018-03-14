1bcff24c862ffd583c986d089202c806dcd39ed4
David Völgyes <david.volgyes@ieee.org>
2018-03-15 00:12:33 +0100

    updated tests

--- tests/test.sh
+++ tests/test.sh
@@ -12,16 +12,31 @@ $PYTHON $DIR/../src/dcm2hdr.py -h
 # Dean Vaughan, http://deanvaughan.org/
 #
 
+# expected failed tests
+$PYTHON $DIR/../src/dcm2hdr.py x && false || true
+$PYTHON $DIR/../src/dcm2hdr.py x x && false || true
+$PYTHON $DIR/../src/dcm2hdr.py x x x && false || true
+$PYTHON $DIR/../src/dcm2hdr.py https://google.com x && false || true
+
 wget -c -i ${DIR}/example_dicom_files.url -nv
 
 for file in *dcm; do
-    echo $file
-    output=${file/dcm/png}
-    echo "Convert: $output to PNG"
-    $PYTHON $DIR/../src/dcm2hdr.py $file $output
-    output=${file/dcm/tiff}
-    echo "Convert: output to TIFF"
-    $PYTHON $DIR/../src/dcm2hdr.py $file $output
-    echo "$output is done."
+  echo $file
+  output=${file/dcm/png}
+  echo "Convert: $output to PNG"
+  $PYTHON $DIR/../src/dcm2hdr.py $file $output
+  output=${file/dcm/tiff}
+  echo "Convert: output to TIFF"
+  $PYTHON $DIR/../src/dcm2hdr.py $file $output
+  echo "$output is done."
 done
+
+$PYTHON $DIR/../src/dcm2hdr.py $file $output -R
+$PYTHON $DIR/../src/dcm2hdr.py $file $output -R -m -1000 -M 2000
+$PYTHON $DIR/../src/dcm2hdr.py $file $output -R -S -r 2 --offset -1000
+$PYTHON $DIR/../src/dcm2hdr.py $file $output  -z 0 -s
+
 rm -f *.png *.tiff *.dcm
+echo
+echo
+echo "  TESTS ARE OK!  "
