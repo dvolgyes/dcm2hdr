8302a1d03a6813f4c077f3ab774a4140a112408f
David Völgyes <david.volgyes@ieee.org>
2018-03-14 21:58:13 +0100

    test script fix

--- tests/test.sh
+++ tests/test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
+
+PYTHON="python -m coverage run -a --source ../src"
+$PYTHON ../src/dcm2hdr.py
+$PYTHON ../src/dcm2hdr.py -h
+
 #
 # Test dicom files are downloaded from: http://deanvaughan.org/wordpress/2013/07/dicom-sample-images/
 # The website is under Creative Commons 4.0 Attribution - Non-commercial license, so here is the credit:
 # Dean Vaughan, http://deanvaughan.org/
 #
-wget -c -i example_dicom_files.url -nv
 
-PYTHON=python -m coverage run -a --source ../src
+wget -c -i example_dicom_files.url -nv
 
 for file in *dcm; do
     echo $file
@@ -15,11 +19,8 @@ for file in *dcm; do
     echo "Convert: $output to PNG"
     $PYTHON ../src/dcm2hdr.py $file $output
     output=${file/dcm/tiff}
-    echo "$Convert: output to TIFF"
+    echo "Convert: output to TIFF"
     $PYTHON ../src/dcm2hdr.py $file $output
     echo "$output is done."
 done
 rm -f *.png *.tiff *.dcm
-
-$PYTHON python ../src/dcm2hdr.py
-$PYTHON python ../src/dcm2hdr.py -h
