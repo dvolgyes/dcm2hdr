535f639051d78dbed5b5b9adef27ad5163ebc675
David Völgyes <david.volgyes@ieee.org>
2018-03-28 09:11:55 +0200

    appveyor error codes update

--- tests/win-test.bat
+++ tests/win-test.bat
@@ -6,20 +6,34 @@ REM #
 
 REM # expected failed tests
 cd tests
-dir
 wget -c -i example_dicom_files.url -nv
-python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm -h
-python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm --version
-python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm        out.png
-python ../src/dcm2hdr.py CT-MONO2-16-brain.dcm        out.tiff -R
-python ../src/dcm2hdr.py xr_hands.dcm                 out.png  -m -1000
-python ../src/dcm2hdr.py xr_skull.dcm                 out.tiff -M 1500
-python ../src/dcm2hdr.py xr_tspine.dcm                out.png  -r 2 -S
-python ../src/dcm2hdr.py xr_chest.dcm                 out.tiff -s 
-python ../src/dcm2hdr.py MR-MONO2-8-16x-heart.dcm     out.png  -o 2000 
-python ../src/dcm2hdr.py US-MONO2-8-8x-execho.dcm     out.tiff
-python ../src/dcm2hdr.py US-RGB-8-epicard.dcm         out.png
-python ../src/dcm2hdr.py US-RGB-8-esopecho.dcm        out.tiff
-python ../src/dcm2hdr.py US-PAL-8-10x-echo.dcm        out.png
+if %errorlevel% neq 0 exit /b %errorlevel%
 
-echo "  TESTS ARE OK!  "
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm -h
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm --version
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm        out1.png
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py CT-MONO2-16-brain.dcm        out2.tiff -R
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py xr_hands.dcm                 out3.png  -m -1000
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py xr_skull.dcm                 out4.tiff -M 1500
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py xr_tspine.dcm                out5.png  -r 2 -S
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py xr_chest.dcm                 out6.tiff -s 
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py MR-MONO2-8-16x-heart.dcm     out7.png  -o 2000 
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py US-MONO2-8-8x-execho.dcm     out8.tiff
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py US-RGB-8-epicard.dcm         out9.png
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py US-RGB-8-esopecho.dcm        out10.tiff
+if %errorlevel% neq 0 exit /b %errorlevel%
+python ../src/dcm2hdr.py US-PAL-8-10x-echo.dcm        out11.png
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+dir
