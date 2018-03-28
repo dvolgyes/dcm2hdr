b775744bb8dfbb32586d750f4550d6bc1af5e78d
David Völgyes <david.volgyes@ieee.org>
2018-03-28 09:05:00 +0200

    appveyor test update

--- tests/win-test.bat
+++ tests/win-test.bat
@@ -5,7 +5,21 @@ REM # Dean Vaughan, http://deanvaughan.org/
 REM #
 
 REM # expected failed tests
+cd tests
 dir
-wget -c -i ${DIR}/example_dicom_files.url -nv
+wget -c -i example_dicom_files.url -nv
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm -h
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm --version
+python ../src/dcm2hdr.py CT-MONO2-16-ankle.dcm        out.png
+python ../src/dcm2hdr.py CT-MONO2-16-brain.dcm        out.tiff -R
+python ../src/dcm2hdr.py xr_hands.dcm                 out.png  -m -1000
+python ../src/dcm2hdr.py xr_skull.dcm                 out.tiff -M 1500
+python ../src/dcm2hdr.py xr_tspine.dcm                out.png  -r 2 -S
+python ../src/dcm2hdr.py xr_chest.dcm                 out.tiff -s 
+python ../src/dcm2hdr.py MR-MONO2-8-16x-heart.dcm     out.png  -o 2000 
+python ../src/dcm2hdr.py US-MONO2-8-8x-execho.dcm     out.tiff
+python ../src/dcm2hdr.py US-RGB-8-epicard.dcm         out.png
+python ../src/dcm2hdr.py US-RGB-8-esopecho.dcm        out.tiff
+python ../src/dcm2hdr.py US-PAL-8-10x-echo.dcm        out.png
 
 echo "  TESTS ARE OK!  "
